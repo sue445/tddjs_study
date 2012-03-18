790d0fb5c8bed1d3c7561b8dd63ce24b3f951092
sue445 <sue445@sue445.net>
2012-03-18 22:05:11 +0900

    add test

---
+++ test/tddjs_test.js
@@ -0,0 +1,36 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: sue445
+ * Date: 12/03/18
+ * Time: 21:34
+ * To change this template use File | Settings | File Templates.
+ */
+TestCase("NamespaceTest", {
+    tearDown: function(){
+        delete tddjs.nstest;
+    },
+
+    "test should create non-existent object" :
+        function(){
+            tddjs.namespace("nstest");
+            assertObject(tddjs.nstest);
+        },
+
+    "test should not overwrite existing objects" :
+        function(){
+            tddjs.nstest = {nested : {}};
+            var result = tddjs.namespace("nstest.nested");
+
+            assertSame(tddjs.nstest.nested, result);
+        },
+
+    "test only create missing parts" :
+        function(){
+            var existing = {};
+            tddjs.nstest = {nested: {existing : existing}};
+            var result = tddjs.namespace("nstest.nested.ui");
+
+            assertSame(existing, tddjs.nstest.nested.existing);
+            assertObject(tddjs.nstest.nested.ui);
+        }
+});
