bede5bb04716592e89786bbf7fc86b7ad508c4aa
sue445 <sue445@sue445.net>
2012-03-20 18:44:35 +0900

    support notifyObservers arguments

--- src/observable.js
+++ src/observable.js
@@ -35,7 +35,7 @@ tddjs.namespace("util");
 
     function notifyObservers(){
         for(var i = 0, l = this.observers.length; i < l; i++){
-            this.observers[i]();
+            this.observers[i].apply(this, arguments);
         }
     }
 
--- test/observable_test.js
+++ test/observable_test.js
@@ -39,5 +39,19 @@ TestCase("ObservableNotifyObserversTest", {
 
         assertTrue(observer1.called);
         assertTrue(observer2.called);
+    },
+
+    "test should pass through arguments" : function(){
+        var observable = new tddjs.util.Observable();
+        var actual;
+
+        observable.addObserver(function(){
+            actual = arguments;
+        });
+
+        observable.notifyObservers("String", 1, 32);
+
+        assertEquals(["String", 1, 32], actual);
     }
+
 });
