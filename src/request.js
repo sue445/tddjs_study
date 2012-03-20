5aae0092e28aeb4045ddc5137497375574553145
sue445 <sue445@sue445.net>
2012-03-21 00:13:12 +0900

    add test: request GetRequestTest

---
+++ src/request.js
@@ -0,0 +1,8 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: sue445
+ * Date: 12/03/21
+ * Time: 0:08
+ * To change this template use File | Settings | File Templates.
+ */
+tddjs.namespace("ajax").get = function(){};
---
+++ test/request_test.js
@@ -0,0 +1,14 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: sue445
+ * Date: 12/03/21
+ * Time: 0:08
+ * To change this template use File | Settings | File Templates.
+ */
+TestCase("GetRequestTest", {
+    "test should define get method" : function(){
+        assertFunction(tddjs.ajax.get);
+    }
+
+    // TODO P254までやった
+});
