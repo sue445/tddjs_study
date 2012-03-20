926b6aa46b1de18340c14e0657019b543c3081ec
sue445 <sue445@sue445.net>
2012-03-20 23:10:15 +0900

    impl isHostMethod

--- src/tdd.js
+++ src/tdd.js
@@ -26,3 +26,16 @@ var tddjs = (function(){
         namespace : namespace
     };
 }());
+
+
+tddjs.isHostMethod = (function(){
+    function isHostMethod(object, property){
+        var type = typeof object[property];
+
+        return type = "function" ||
+            (type == "object" && !object[property]) ||
+            type == "unknown";
+    }
+
+    return isHostMethod;
+}());
