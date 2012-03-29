ac3544aff9ebf299e915db57e23605c7827df446
sue445 <sue445@sue445.net>
2012-03-30 00:25:51 +0900

    P276: refactor ajax.get

--- src/request.js
+++ src/request.js
@@ -36,20 +36,9 @@ tddjs.noop = function(){};
 //    }
 
     function get(url, options){
-        if(typeof url != "string"){
-            throw new TypeError("URL should be string");
-        }
-
         options = options || {};
-        var transport = tddjs.ajax.create();
-        transport.open("GET", url, true);
-        transport.onreadystatechange = function(){
-            if(transport.readyState == 4){
-                requestComplete(transport, options);
-                transport.onreadystatechange = tddjs.noop;
-            }
-        };
-        transport.send(null);
+        options.method = "GET";
+        ajax.request(url, options);
     }
 
     ajax.get = get;
