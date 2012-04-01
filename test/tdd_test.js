57e8f8e2789d5dbd6819cbcb1bee1dab1a7c113b
sue445 <sue445@sue445.net>
2012-04-02 00:30:22 +0900

    P164 impl tddjs.extend

--- test/tdd_test.js
+++ test/tdd_test.js
@@ -43,3 +43,25 @@ TestCase("NamespaceTest", {
             assertUndefined(tddjs.dom);
         }
 });
+
+TestCase("ObjectExtendTest", {
+   setUp : function(){
+       this.dummy = {
+           setName : function(name){
+               return (this.name == name);
+           },
+
+           getName : function(){
+               return this.name || null;
+           }
+       };
+   },
+
+    "test should copy properties" : function(){
+        var object = {};
+        tddjs.extend(object, this.dummy);
+
+        assertEquals("function", typeof object.getName);
+        assertEquals("function", typeof object.setName);
+    }
+});
